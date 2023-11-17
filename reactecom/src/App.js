5dc8f7f2dd2c231176fd64200761acc391a76fae
waliullahcoder <waliullahbiplob786@gmail.com>
2023-11-17 09:18:43 +0600

    backend and frontend blading

---
+++ reactecom/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MasterLayout from './layouts/admin/MasterLayout';
+import routes from './routes/routes';
+import Home from './components/frontend/Home';
+import Login from './components/frontend/auth/Login';
+import Register from './components/frontend/auth/Register';
+function App() {
+  return (
+    <div className="App">
+    <Router>
+      <Routes>
+      <Route path="/" Component={Home} />
+      <Route path="/login" Component={Login} />
+      <Route path="/register" Component={Register} />
+        <Route path="/" element={<MasterLayout />}>
+          {routes.filter(route => route.component)
+          .map(({ path, component: Component }) => (
+            <Route
+              key={path}
+              path={path}
+              element={<Component />}
+            />
+          )
+          )}
+        
+        </Route>
+      </Routes>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
--- src/routes/routes.js
+++
@@ -1,9 +0,0 @@
-
-import Dashboard from '../components/Dashboard';
-import Profile from '../components/Profile';
-
-const routes =[
-  { path: '/admin/dashboard', exact: true, name: 'Dashboard', component: Dashboard},
-  { path: '/admin/profile', exact: true, name: 'Profile', component: Profile},
-];
-export default routes;
